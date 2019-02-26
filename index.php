2932dd7e600633ce89ed2aeae3eeb6a366e4993b
pointbaaar <alex23667@gmail.com>
2019-02-26 15:24:06 +0100

    use media queries for mobile responsive

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <html>
 <head>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <!--
+    <link
+            rel="stylesheet"
+            href="style-mobile.css"
+            media="screen and (max-width: 20cm) and (min-width: 1cm)"
+    >
+    <link
+            rel="stylesheet"
+            href="style-desktop.css"
+            media="screen and (max-width: 9999cm) and (min-width: 20cm)"
+    >
+    -->
+    <link
+            rel="stylesheet"
+            href="style-mobile.css"
+            media="screen and (orientation:portrait)"
+    >
+    <link
+            rel="stylesheet"
+            href="style-desktop.css"
+            media="screen and (orientation:landscape)"
+    >
 </head>
 <body>
     <?php
@@ -10,10 +32,11 @@
     ?>
 
   <div class="container mycontainer">
-    hello
-    <p>
-      Afropean
-    </p>
+    <div class="m-3"></div>
+    <img
+            src="https://tickets.afropeanbase.com/wp-content/uploads/2019/02/https_cdn.evbuc_.com_images_56062838_220840326330_1_original.20190131-220610.jpeg"
+            style="width:80%; "
+    />
   </div>
 </body>
 </html>
