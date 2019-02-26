03d1a85cc6a0c7bee6dd9db116f36d6fc40aabdc
pointbaaar <alex23667@gmail.com>
2019-02-26 15:26:30 +0100

    refactor repetitive code into fragment

--- index.php
+++ index.php
@@ -1,42 +1,18 @@
 <html>
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-
-    <!--
-    <link
-            rel="stylesheet"
-            href="style-mobile.css"
-            media="screen and (max-width: 20cm) and (min-width: 1cm)"
-    >
-    <link
-            rel="stylesheet"
-            href="style-desktop.css"
-            media="screen and (max-width: 9999cm) and (min-width: 20cm)"
-    >
-    -->
-    <link
-            rel="stylesheet"
-            href="style-mobile.css"
-            media="screen and (orientation:portrait)"
-    >
-    <link
-            rel="stylesheet"
-            href="style-desktop.css"
-            media="screen and (orientation:landscape)"
-    >
-</head>
+<?php
+    include("include.fragment.html");
+?>
 <body>
-    <?php
-        include("header.html");
-    ?>
+<?php
+    include("header.fragment.html");
+?>
 
-  <div class="container mycontainer">
-    <div class="m-3"></div>
-    <img
-            src="https://tickets.afropeanbase.com/wp-content/uploads/2019/02/https_cdn.evbuc_.com_images_56062838_220840326330_1_original.20190131-220610.jpeg"
-            style="width:80%; "
-    />
-  </div>
+<div class="container mycontainer">
+<div class="m-3"></div>
+<img
+        src="https://tickets.afropeanbase.com/wp-content/uploads/2019/02/https_cdn.evbuc_.com_images_56062838_220840326330_1_original.20190131-220610.jpeg"
+        style="width:80%; "
+/>
+</div>
 </body>
 </html>
