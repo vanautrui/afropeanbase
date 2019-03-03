e487504e0a455550865ee09fea75d2501378b64f
pointbaaar <alex23667@gmail.com>
2019-03-03 14:24:37 +0100

    add some notes

--- media.php
+++ media.php
@@ -21,8 +21,14 @@
             <iframe width="560" height="315" src="https://www.youtube.com/embed/3x74SF4Ks1E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/6IzFAf4dGs0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/SvLtsAkFGZQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <!-- TODO: embed the videos of Yxng Bane and other artists. Maybe sort them by age if there is time.-->
+            <!-- maybe there should also be a page 'albums.php' where people can hear excerpts of the various albums and songs that the artists have produced.
+            if there is no music video on youtube -->
         </div>
     </div>
+
+
 
 </body>
 </html>
