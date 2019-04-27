d51c04e6c1c5d29a5bea6c560a0a2ed308310ccb
k9ne257 <alex23667@gmail.com>
2019-04-28 00:15:29 +0200

    Fade added to Homepage

--- index.php
+++ index.php
@@ -3,25 +3,33 @@
     include("include.fragment.html");
 ?>
 <body>
-<?php
-    include("header.fragment.html");
-?>
+<div class="onboard">
+    <img src="assets/bigger_cut.png">
+</div>
 
-<div class="mycontainer container-fluid">
-    <div class="m-3"></div>
-    <h1 class="text-center">
-        Upcoming Events:
-    </h1>
-    <div class="row justify-content-center">
-        <img
-                src="https://tickets.afropeanbase.com/wp-content/uploads/2019/02/https_cdn.evbuc_.com_images_56062838_220840326330_1_original.20190131-220610.jpeg"
-                style="max-width:100%;"
-        />
+<div class="offboard">
+    <?php
+        include("header.fragment.html");
+    ?>
+
+
+    <div class="mycontainer container-fluid">
+        <div class="m-3"></div>
+        <h1 class="text-center">
+            Upcoming Events:
+        </h1>
+        <div class="row justify-content-center">
+            <img
+                    src="https://tickets.afropeanbase.com/wp-content/uploads/2019/02/https_cdn.evbuc_.com_images_56062838_220840326330_1_original.20190131-220610.jpeg"
+                    style="max-width:100%;"
+            />
+        </div>
+        <hr>
+        <h1 class="text-center">
+            Past Events:
+        </h1>
     </div>
-    <hr>
-    <h1 class="text-center">
-        Past Events:
-    </h1>
+
 </div>
 </body>
 </html>
